0f0bb39dae966e211e866d124e1bf4d4024a8678
achraf.boujaanan <boujaananachraf@gmail.com>
2024-04-12 13:31:37 +0200

    Componente ts01 completado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Angular01Component } from './angular01/angular01.component';
 import { Reto1Component } from './reto1/reto1.component';
 import { Reto2Component } from './reto2/reto2.component';
+import { Ts01Component } from './ts01/ts01.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Reto2Component],
+  imports: [RouterOutlet, Ts01Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
